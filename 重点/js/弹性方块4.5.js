8f75d130121211d83e00cd40c45f28f08a723fbf
okaooz2 <okaooz2@163.com>
2017-11-06 10:53:19 +0800

    00

--- 重点/js/弹性方块4.5.js
+++ 重点/js/弹性方块4.5.js
@@ -89,13 +89,13 @@ function buildBounceBox() {
                         }, false);
                         break;
                     case 2: 
-                        buttons[i].style = "right: 0; top: 100%;";
+                        buttons[i].style = "right: 0; top: 100%; transform: translate(0, -4px);";  // //不知道为什么画布的父元素在y方向上多出了一点不能包围画布，故做一个平移。下同
                         buttons[i].addEventListener("click", function(event) {
                             all_squares.addOneSquare(perameter_data.canvas_width - perameter_data.side_length, perameter_data.canvas_height - perameter_data.side_length);
                         }, false);
                         break;
                      case 3: 
-                        buttons[i].style = "left: 0; top: 100%;";
+                        buttons[i].style = "left: 0; top: 100%; transform: translate(0, -4px);";
                         buttons[i].addEventListener("click", function(event) {
                             all_squares.addOneSquare(0, perameter_data.canvas_height - perameter_data.side_length);
                         }, false);
